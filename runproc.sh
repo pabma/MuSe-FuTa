877d94e779843d7afb3ff73b2661b572c639ff10
Pablo Martinez <pmmgabas@yahoo.es>
2024-12-13 16:16:01 +0100

    Add files via upload

--- runproc.sh
+++ runproc.sh
@@ -6,13 +6,13 @@ fi
 
 for f in used/*.gz; do
 
-	python3 preproc.py -o ${f%.nii.gz} $1 $2 $3 $4  # ORIGINAL with Moose
-#	python3 preproc_TSyPT.py -o ${f%.nii.gz} $1 $2 $3 $4  # Without Moose
+	python3 runproc/preproc.py -o ${f%.nii.gz} $1 $2 $3 $4  # ORIGINAL with Moose
+#	python3 runproc/preproc_TSyPT.py -o ${f%.nii.gz} $1 $2 $3 $4  # Without Moose
 
-	python3 mix_img.py -o ${f%.nii.gz} $1 $2 $3 $4  # ORIGINAL
-#	python3 mix_img_TSyPT.py -o ${f%.nii.gz} $1 $2 $3 $4 # Platipy y TotalSegmentator
+	python3 runproc/mix_img.py -o ${f%.nii.gz} $1 $2 $3 $4  # ORIGINAL
+#	python3 runproc/mix_img_TSyPT.py -o ${f%.nii.gz} $1 $2 $3 $4 # Platipy y TotalSegmentator
 
-#	python3 postproc_valves.py -o ${f%.nii.gz}
+	python3 runproc/postproc_valves.py -o ${f%.nii.gz}
 
 done
 
